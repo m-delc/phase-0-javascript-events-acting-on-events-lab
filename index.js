e5673e912e0829fd1f75e671cea52f4324e1a96d
MichaelDelcambre <delcambrem@gmail.com>
2021-11-11 14:54:02 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ function moveDodgerLeft() {
   };
 }
 
+document.addEventListener("keydown"), function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+    };
+}
+
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
 
     if (left > 0) {
         dodger.style.left = `${left + 1}px`;
+    };
+};
+
+document.addEventListener("keydown"), function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
     };
 }
